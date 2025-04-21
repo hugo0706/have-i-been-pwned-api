97089c89c1655b0460ffe2b17ce3eb2154961247
hugo0706 <hugo.garciac@edu.uah.es>
2025-04-21 15:39:40 +0200

    Remove ntlm option

--- lib/have_i_been_pwned_api/endpoints/pwned_passwords/check_pwd.rb
+++ lib/have_i_been_pwned_api/endpoints/pwned_passwords/check_pwd.rb
@@ -6,42 +6,30 @@ require "digest"
 module HaveIBeenPwnedApi
   module PwnedPasswords
     class CheckPwd < Endpoint
-      ALLOWED_PARAMS = %i[mode]
-
       def self.type
         :free
       end
 
-      def self.call(password:, add_padding: false, **kwargs)
-        ntlm_mode = kwargs["mode"]&.downcase == "ntlm"
-        digest = calculate_digest(password, ntlm_mode)
+      def self.call(password:, add_padding: false)
+        digest = hash_password(password)
 
-        params = parse_optional_params(kwargs, ALLOWED_PARAMS)
-        response = Client.get(uri(digest[..4], params),
+        response = Client.get(uri(digest[..4]),
                               headers: { add_padding: add_padding })
 
-        matches = response.split("\r\n").select do |val|
-          val.split(":").first == digest[5..]
-        end
+        partial_hash = Regexp.escape(digest[5..])
+        count = response.body.match(/#{partial_hash}:(\d+)/) { $1.to_i }
 
-        matches.empty? ? false : true
+        count.nil? ? false : true
       end
 
       private
 
-      def self.calculate_digest(password, ntlm_mode)
-        if ntlm_mode
-          utf16le = password.encode("UTF-16LE")
-          Digest::MD4.digest(utf16le).unpack1("H*").upcase
-        else
-          Digest::SHA1.hexdigest(password).upcase
-        end
+      def self.hash_password(password)
+        Digest::SHA1.hexdigest(password).upcase
       end
 
-      def self.uri(digest_chars, params)
-        uri = URI("#{endpoint_url}range/#{digest_chars}")
-        uri.query = URI.encode_www_form(params) unless params.empty?
-        uri
+      def self.uri(digest_chars)
+        URI("#{endpoint_url}range/#{digest_chars}")
       end
     end
   end
